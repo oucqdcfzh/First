ced16359ffe2700d60cfdb7533e37875bed97adc
oucqdcfzh <54127613@qq.com>
2019-09-17 09:54:35 +0800

    Second

--- src/main/java/MyServlet.java
+++ src/main/java/MyServlet.java
@@ -1,4 +1,8 @@
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * @program First
@@ -8,4 +12,8 @@ import javax.servlet.http.HttpServlet;
  **/
 
 public class MyServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
 }
